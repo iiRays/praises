f80836429eed9a37fa575d3a59c49d747eaa52ba
Johann Lee <mast3r1sgr3at@hotmail.com>
2021-01-06 16:42:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,22 @@
 var countDownDate = new Date()
 countDownDate.setHours(14);
 countDownDate.setMinutes(30);
+countDownDate.setSeconds(0);
 
 // Get today's date and time
 var now = new Date().getTime();
 
 // Set date to tmr if already over
-if (Math.floor((countDownDate.getTime() - now % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) <= -4) {
-    countDownDate.setDate(date.getDate() + 1);
+if (Math.floor(((countDownDate.getTime() - now) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) <= -2) {
+    countDownDate.setDate(countDownDate.getDate() + 1);
 }
+
+
 
 // Update the count down every 1 second
 var x = setInterval(function () {
 
+    now = new Date().getTime();
 
     // Find the distance between now and the count down date
     var distance = countDownDate.getTime() - now;
@@ -33,8 +37,11 @@ var x = setInterval(function () {
     $("#minCount").text(minutes < 0 ? 0 : minutes);
     $("#secCount").text(seconds < 0 ? 0 : seconds);
 
+    console.log(seconds);
+
+
     // While the session is active
-    if (distance < 0 && hours > -4) {
+    if (distance < 0 && hours > -2) {
         $('#zoomBt').click(function(){
             window.location.assign("https://us02web.zoom.us/j/7461305803?pwd=Yms2L0Q2RzhteVhvV2pIYkVDbzAydz09");
         });
